410568d38431c7d4f1728cb44134b977fe716205
Dustin Sims <dustinsims0@gmail.com>
2021-04-26 12:38:09 -0500

    Improve env handling, prep alembic for env variables

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,5 @@
+import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,6 +7,11 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from database.models import Base
+from dotenv import load_dotenv
+
+load_dotenv()
+DB
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
--- database/__init__.py
+++ database/__init__.py
@@ -5,9 +5,10 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
-POSTGRES_USER = os.getenv('POSTGRES_USER')
-POSTGRES_PASS = os.getenv('POSTGRES_PASS')
-POSTGRES_URL = os.getenv('POSTGRES_URL')
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DB_URL = os.getenv('DB_URL')
+DB_NAME = os.getenv('DB_NAME')
 
-engine = create_engine(f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASS}@{POSTGRES_URL}", echo=True)
+engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_URL}/{DB_NAME}", echo=True)
 Session = sessionmaker(bind=engine)
