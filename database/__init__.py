73a2a868806e284a9c78d37ed14593c9b3a49ab2
Dustin Sims <dustinsims0@gmail.com>
2021-04-26 09:24:24 -0500

    Remove local db info and move it to env

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-engine = create_engine("postgresql+psycopg2://postgres:Pumph0use!!@localhost:5432/rocketrandy", echo=True)
+load_dotenv()
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASS = os.getenv('POSTGRES_PASS')
+POSTGRES_URL = os.getenv('POSTGRES_URL')
+
+engine = create_engine(f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASS}@{POSTGRES_URL}", echo=True)
 Session = sessionmaker(bind=engine)
